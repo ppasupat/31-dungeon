5bea64db2ac38782eae25f62a68434f2105004a7
Ice Pasupat <panuponggong@gmail.com>
2024-02-17 21:28:42 -0800

    Rooms 4 and 5

--- data.js
+++ data.js
@@ -32,7 +32,7 @@ const [MAP_DATA, NPC_DATA] = function () {
   }
 
   // ################################
-  // Data: Past
+  // Room 0
 
   // p0
   map_data.p0 = {
@@ -63,6 +63,7 @@ const [MAP_DATA, NPC_DATA] = function () {
           utils.removeItem('sword');
           [
             'dwarfIdle', 'dwarfWorking', 'timeTravelerFirst',
+            'bridgeFrame', 'bridge',
           ].map(utils.refreshNpcOnMap);
           return R(2, false, false, [
             'โอ้! ดาบนี้ตัดไม้ได้ดีจริง ๆ เดี๋ยวข้าเริ่มงานเลยแล้วกัน',
@@ -70,36 +71,6 @@ const [MAP_DATA, NPC_DATA] = function () {
       }
     },
   };
-
-  // p1
-  map_data.p1 = {
-    pid: 'p1', row: 0, col: 1,
-    arrows: {'w': 'p0'},
-    mainNpc: 'dwarfWorking',
-  };
-
-  npc_data.dwarfWorking = {
-    nid: 'dwarfWorking', loc: 'p1',
-    nidAlias: 'dwarf',
-    name: 'คนแคระ',
-    actionText: '',
-    itemText: XXX,
-    mapStates: {'swordGiven': 'appear'},
-    content: function (op, flags, utils) {
-      return R(0, false, false, [
-        'รอหน่อยนะ คงใช้เวลาก่อสร้างสักพัก',
-      ]);
-    },
-  };
-
-  // p3
-  map_data.p3 = {
-    pid: 'p3', row: 1, col: 1,
-    arrows: {},
-  };
-
-  // ################################
-  // Data: Future
 
   // f0 (start)
   map_data.f0 = {
@@ -132,6 +103,30 @@ const [MAP_DATA, NPC_DATA] = function () {
     },
   };
 
+  // ################################
+  // Room 1
+
+  // p1
+  map_data.p1 = {
+    pid: 'p1', row: 0, col: 1,
+    arrows: {'w': 'p0'},
+    mainNpc: 'dwarfWorking',
+  };
+
+  npc_data.dwarfWorking = {
+    nid: 'dwarfWorking', loc: 'p1',
+    nidAlias: 'dwarf',
+    name: 'คนแคระ',
+    actionText: '',
+    itemText: XXX,
+    mapStates: {'swordGiven': 'appear'},
+    content: function (op, flags, utils) {
+      return R(0, false, false, [
+        'รอหน่อยนะ คงใช้เวลาก่อสร้างสักพัก',
+      ]);
+    },
+  };
+
   // f1
   map_data.f1 = {
     pid: 'f1', row: 0, col: 3,
@@ -161,10 +156,112 @@ const [MAP_DATA, NPC_DATA] = function () {
     },
   }
 
+  // ################################
+  // Room 3
+
+  // p3
+  map_data.p3 = {
+    pid: 'p3', row: 1, col: 1,
+    arrows: {'s': 'p5'},
+  };
+
+  npc_data.bridgeFrame = {
+    nid: 'bridgeFrame', loc: 'p3', cosmetic: true,
+    name: 'bridgeFrame',
+    mapStates: {'swordGiven': 'appear'},
+  };
+
   // f3
   map_data.f3 = {
     pid: 'f3', row: 1, col: 3,
-    arrows: {'n': 'f1'},
+    arrows: {'n': 'f1', 's': 'f5'},
+  };
+
+  npc_data.bridge = {
+    nid: 'bridge', loc: 'f3', cosmetic: true,
+    name: 'bridge',
+    mapStates: {'swordGiven': 'appear'},
+  };
+
+  // ################################
+  // Room 4
+
+  // p4
+  map_data.p4 = {
+    pid: 'p4', row: 2, col: 0,
+    arrows: {'e': 'p5'},
+    mainNpc: 'aristocrat',
+  };
+
+  npc_data.aristocrat = {
+    nid: 'aristocrat', loc: 'p4',
+    name: 'ขุนนาง',
+    actionText: 'ขอตังหน่อย',
+    itemText: XXX,
+    content: function (op, flags, utils) {
+      switch (op) {
+        case 'enter':
+          return R(0, true, false, [
+            'เธอเป็นใคร!? จะมาขโมยสมบัติฉันเหรอ?',
+          ]);
+        case 'action':
+          return R(0, true, false, [
+            'ฉันไม่ให้สมบัติเธอง่าย ๆ หรอก!',
+          ]);
+      }
+    },
+  };
+
+  // f4
+  map_data.f4 = {
+    pid: 'f4', row: 2, col: 2,
+    arrows: {},
+    mainNpc: 'safe',
+  };
+
+  npc_data.safe = {
+    nid: 'safe', loc: 'f4',
+    name: 'ตู้เซฟ',
+    actionText: 'จิ๊กของ',
+    itemText: XXX,
+    mapStates: {'safeOpen': 'map-safe-1'},
+    content: function (op, flags, utils) {
+      switch (op) {
+        case 'enter':
+          return R(flags.safeOpen || 0, true, false, [
+            'ตู้เซฟ ดูแข็งแรง',
+          ]);
+        case 'action':
+          if (!flags.safeOpen) {
+            return R(0, true, false, [
+              'ตู้เซฟปิดอยู่ ต้องเปิดมันก่อน',
+            ]);
+          } else if (!flags.teaTaken) {
+            return R(1, true, false, [
+              'คุณหยิบ <b>ใบชา</b> จากตู้เซฟ',
+            ]);
+          } else {
+            return R(1, true, false, [
+              'พอแล้วลูก งกจริง',
+            ]);
+          }
+      }
+    }
+  };
+
+  // ################################
+  // Room 5
+
+  // p5
+  map_data.p5 = {
+    pid: 'p5', row: 2, col: 1,
+    arrows: {'n': 'p3', 'w': 'p4'},
+  };
+
+  // f5
+  map_data.f5 = {
+    pid: 'f5', row: 2, col: 3,
+    arrows: {'n': 'f3'},
   };
   
   return [map_data, npc_data];
